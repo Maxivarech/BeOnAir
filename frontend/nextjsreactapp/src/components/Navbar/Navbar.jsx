63f21d36a9a252415827e063e86ecc7b499df06d
maxime.vanrechem@student.vinci.be <Ourson=2000>
2023-11-25 11:51:10 +0100

    logo NavBar

--- frontend/nextjsreactapp/src/components/Navbar/Navbar.jsx
+++ frontend/nextjsreactapp/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Image from 'next/image';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import logo from '/public/BeOnAirGrey.png'
 
 const NavBar = () => {
 
@@ -22,6 +24,7 @@ const NavBar = () => {
       <Navbar bg="black" variant="dark">
         <Container fluid>
           <Nav className="me-auto">
+              <Image src={logo} width={100} />
               <Nav.Link href="/">All</Nav.Link>
               <NavDropdown title="Link" id="navbarScrollingDropdown" >
                   {series.map((serie)=>(
--- frontend/standardreactapp/src/components/NavBar/NavBar.jsx
+++ frontend/standardreactapp/src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../Img/BeOnAirGrey.png'
 
 const NavBar = ({series}) => {
   console.log(series);
@@ -9,6 +10,7 @@ const NavBar = ({series}) => {
       <Navbar bg="black" variant="dark">
         <Container fluid>
           <Nav className="me-auto">
+              <img src={logo} width={100}/>
               <Nav.Link href="/">All</Nav.Link>
               <NavDropdown title="Link" id="navbarScrollingDropdown" >
                   {series.map((serie)=>(
